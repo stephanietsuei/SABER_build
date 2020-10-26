65bd7d62a1ce3ddf55a29afa66669c489a3fe49c
alexander <aschperb@gmail.com>
2020-10-25 18:25:12 -0700

    update reference

---
+++ play_uav_ugv.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export TURTLEBOT3_MODEL="waffle"
+roslaunch sabr_pkg sabr_gazebo_ugv_uav.launch
--- save_map.sh
+++ save_map.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export TURTLEBOT3_MODEL="waffle"
 mkdir -p `pwd`/src/SABR/maps
-rosrun map_server map_saver -f `pwd`/src/SABR/maps/world5
+rosrun map_server map_saver -f `pwd`/src/SABR/maps/world4
 rosnode kill -a; killall -9 rosmaster; killall -9 roscore
 roslaunch sabr_pkg sabr_gazebo_collect.launch
